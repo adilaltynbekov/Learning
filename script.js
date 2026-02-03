a23670e4222bc8fa70779dbbc11b53201e7ccba5
Adil Altynbekov <dlltnbkv@gmail.com>
2026-02-03 17:54:30 +0500

    add: functions finished

--- script.js
+++ script.js
@@ -7,3 +7,23 @@ let price = 10;
 let quantity = 2;
 const totalMessage = `You are buying ${quantity} ${itemName} for a total of $${price * quantity}.`;
 console.log(totalMessage);
+
+const calculateTotal = (price, quantity) => {
+  const total = price * quantity;
+  console.log(`Your total is $${total}`);
+};
+calculateTotal(50, 3);
+
+const secretPassword = "pizza123";
+const checkAccess = (input) => {
+  if (input === secretPassword) {
+    console.log("Access Granted! Welcome.");
+  } else {
+    console.log("Wrong password. Try again.");
+  }
+};
+
+checkAccess("pizza123");
+checkAccess("pizza12345");
+// Access Granted! Welcome.
+// Wrong password. Try again.
