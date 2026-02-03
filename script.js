7ced79351c07be3ca02e1cb0ff95abb85d3cd0ba
Adil Altynbekov <dlltnbkv@gmail.com>
2026-02-03 18:21:52 +0500

    add: finished dom manipulations

--- script.js
+++ script.js
@@ -27,3 +27,31 @@ checkAccess("pizza123");
 checkAccess("pizza12345");
 // Access Granted! Welcome.
 // Wrong password. Try again.
+
+const statusText = document.querySelector("#status-display");
+const myButton = document.querySelector(".action-btn");
+
+statusText.textContent = "User is Online";
+// myButton.style.backgroundColor = "green";
+
+const toggleBtn = document.querySelector(".toggle-btn");
+const makeMagicHappen = () => {
+  document.body.style.backgroundColor = "navy";
+  toggleBtn.textContent = "Done!";
+};
+// toggleBtn.addEventListener("click", makeMagicHappen);
+
+let isDarkMode = false;
+const modeToggle = document.querySelector(".mode-toggle");
+const toggleDarkMode = () => {
+  if (!isDarkMode) {
+    document.body.classList.add("dark-mode");
+    isDarkMode = true;
+    modeToggle.textContent = "Switch to Light Mode";
+  } else {
+    document.body.classList.remove("dark-mode");
+    isDarkMode = false;
+    modeToggle.textContent = "Switch to Dark Mode";
+  }
+};
+modeToggle.addEventListener("click", toggleDarkMode);
